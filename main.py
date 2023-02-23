7c93c3129951850c48be0e55c21f86606cc03cca
LucaPzzl <119593629+LucaPzzl@users.noreply.github.com>
2023-02-23 12:14:04 +0100

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import main_giovanni
-import saluto_alex_foli
-
-def saluto_nome_cognome():
-    print("Salve Luca Pezzolati")
-print(saluto_nome_cognome())
-
-main.giovanni.saluto_nome_cognome()
-main.saluto_alex_foli.saluto_nome_cognome()
